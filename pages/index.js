db9b1784e03e1587c18963b92cd6662f362f1896
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-06-06 10:20:06 +0530

    get all  posts with frontmatter

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
+import fs from "fs";
+import path from "path";
 import Layout from "../components/Layout";
+import matter from "gray-matter";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <Layout>
       <h1>Hello World</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
