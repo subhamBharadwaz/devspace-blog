99902f87c47e73b13fcf37092f9a202408975eb7
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-06-06 17:30:23 +0530

    category pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import fs from "fs";
 import path from "path";
-import Layout from "../components/Layout";
-import Post from "../components/Post";
+import Layout from "@/components/Layout";
+import Post from "@/components/Post";
 import Link from "next/link";
 import matter from "gray-matter";
-import { sortByDate } from "../utils";
+import { sortByDate } from "@/utils/index";
 
-export default function Home({ posts }) {
+export default function HomePage({ posts }) {
   return (
     <Layout>
       <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
